95ca6ba2a9ac893aae7a99ee33fb88f37bf2962d
Hau Dao <hau.vdao@gmail.com>
2017-03-17 13:15:52 +0700

    mailing list

---
+++ app/models/visitor.rb
@@ -0,0 +1,16 @@
+class Visitor < ActiveRecord::Base
+  has_no_table
+  column :email, :string
+  validates_presence_of :email
+  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+
+  def subscribe
+    mailchimp = Gibbon::Request.new
+    result = mailchimp.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {
+      email_address: self.email,
+      status: "subscribed",
+    })
+      Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+    end
+
+  end
